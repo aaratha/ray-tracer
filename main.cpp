ddafccb2be3fcf48167ad20a112dbaa0f380abbe
Aseem Ratha <aseemratha@gmail.com>
2024-10-01 16:51:56 -0400

    header functions, scanlines

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
+#include "color.h"
+#include "vec3.h"
+
 #include <iostream>
 
-auto main(int argc, char *argv[]) -> int {
+// clang++ main.cpp && ./a.out > image.ppm
 
+int main() {
 
-    return 0;
+    // Image
+
+    int image_width = 256;
+    int image_height = 256;
+
+    // Render
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = 0; j < image_height; j++) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush << std::endl;
+        for (int i = 0; i < image_width; i++) {
+            auto pixel_color = color(double(i)/(image_width-1), double(j)/(image_height-1), 0);
+            write_color(std::cout, pixel_color);
+        }
+    }
+
+    std::clog << "\rDone.                       \n";
 }

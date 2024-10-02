1ccd9c7c81482b7905fead77a725b689e31c6acc
Aseem Ratha <aseemratha@gmail.com>
2024-10-02 18:16:13 -0400

    metal, materials, etc.

--- camera.h
+++ camera.h
@@ -1,6 +1,9 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
+#include "hittable.h"
+#include "material.h"
+
 class camera {
   public:
     double aspect_ratio      = 1.0;  // Ratio of image width over height
@@ -83,14 +86,17 @@ class camera {
         return vec3(random_double() - 0.5, random_double() - 0.5, 0);
     }
 
-    color ray_color(const ray& r, int depth, const hittable& world) {
+    color ray_color(const ray& r, int depth, const hittable& world) const {
         if (depth <= 0)
             return color(0,0,0);
         hit_record rec;
 
         if (world.hit(r, interval(0.001, infinity), rec)) {
-            vec3 direction = rec.normal + random_unit_vector();
-            return 0.5 * ray_color(ray(rec.p, direction), depth -1, world);
+            ray scattered;
+            color attenuation;
+            if (rec.mat->scatter(r, rec, attenuation, scattered))
+                return attenuation * ray_color(scattered, depth-1, world);
+            return color(0,0,0);
         }
 
         vec3 unit_direction = unit_vector(r.direction());
--- sphere.h
+++ sphere.h
@@ -5,8 +5,8 @@
 
 class sphere : public hittable {
     public:
-    sphere(const point3& center, double radius) : center(center),
-        radius(std::fmax(0,radius)) {}
+    sphere(const point3& center, double radius, shared_ptr<material> mat)
+        : center(center), radius(std::fmax(0,radius)), mat(mat) {}
 
     bool hit(
         const ray& r,
@@ -36,6 +36,7 @@ class sphere : public hittable {
         rec.p = r.at(rec.t);
         vec3 outward_normal = (rec.p - center) / radius;
         rec.set_face_normal(r, outward_normal);
+        rec.mat = mat;
 
         return true;
     }
@@ -43,6 +44,7 @@ class sphere : public hittable {
     private:
     point3 center;
     double radius;
+    shared_ptr<material> mat;
 };
 
 #endif // SPHERE_H
